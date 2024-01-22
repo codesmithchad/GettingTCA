e9d890e24ebc683f5f9648b717919009bbc91b2d
chad <chadh@tnmeta.io>
2024-01-22 13:52:18 +0900

    Init pjt

---
+++ GettingTCA/GettingTCA/GettingTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  GettingTCAApp.swift
+//  GettingTCA
+//
+//  Created by Chad Hur on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct GettingTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
